68c01e49d6b1a31ea6b0d619abcb3cf9fa289c06
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 17:13:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(muscians,instruments){
-  var empty =({})
+  var empty =[]
   for (var i = 0;i<muscians.lengt;i++){empty.push(musician[i]+'plays'+instruments[i]) }
 return empty
 }
