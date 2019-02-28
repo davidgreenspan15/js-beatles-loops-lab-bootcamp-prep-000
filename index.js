7853e7659d18ae583114bf3d45ceef4a8c60becc
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 17:22:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function johnLennonFacts(facts) {
   var results = [];
   var i = 0;
   while (i < facts.length) {
-    results.push(facts[i] + "!!!");
+    results.push(facts[i] + "!!!")
     i++
   }
   return results}
